d6936678eebe587b9c9320ad090edee2fb676d68
USERNAME <mmdmahdiqasemisa@gmail.com>
2025-04-18 20:33:27 +0430

    main wellcome us

---
+++ libmain.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h> // for Sleep(timeByMiliSec); function to make runtime more slowdow
+void printbydilay(char str[] , int firsdilay , int secenddilay ){
+    int index ;
+    for(index=0 ; str[index]!='\0' ; index++ ){
+        putchar(str[index]);
+        Sleep(firsdilay);
+    }
+    Sleep(secenddilay);
+}
+void wellcome(void){
+    printf("----------------------------------------------------------------------\n");
+    Sleep(2000);
+    printbydilay("|\tHi Pretty Honny ! \n" , 70 , 1000 );
+    printbydilay( "|\tWellcome to Tea_Library_v1.0.0 Our first Modular Project \n|\n" , 70 , 1000 );
+    printbydilay("|\tWhat You Want ? Just Tell Us !\n|\n" , 70 , 1000);
+    printbydilay("|\t1. TeaInfo : For all Tea(books) we have\n",60,900);
+    printbydilay("|\t2. FindTea : For finding a tea(book) in Our lib \n" , 60 , 900);
+    printbydilay("|\t3. AddTea : For adding a tea into Our lib  \n|\n",60 , 900 );
+    printbydilay("|\t4. Exit\n",60,900);
+    printf("----------------------------------------------------------------------\n");
+    printbydilay("loading ",200,0);
+    printf(" ____________________________________________\rloading ");
+    Sleep(500);
+    printbydilay("#############################################",500,0);
+    printf("\r                                                     \r");
+    Sleep(1000);
+}
+int main(void){
+    wellcome();
+    return 0 ;
+}
+
+
